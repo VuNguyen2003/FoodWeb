baa6c28a4e955dc9e125a48d425332a041b083ba
Vũ Nguyễn <116508377+VuNguyen2003@users.noreply.github.com>
2024-10-26 08:21:41 +0700

    new commit

--- src/main/java/com/AVfood/foodweb/FoodwebApplication.java
+++ src/main/java/com/AVfood/foodweb/FoodwebApplication.java
@@ -4,7 +4,5 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoodwebApplication {
-
-	public static void main(String[] args) {SpringApplication.run(FoodwebApplication.class, args);}
+public class FoodwebApplication { public static void main(String[] args) {SpringApplication.run(FoodwebApplication.class, args);}
 }
